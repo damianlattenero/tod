e615f851e1b3b13c8bca848f3b5bd2bd337b2dd9
Cravacuore <cravacuore@hotmail.com>
2014-11-09 00:00:08 -0300

    Resolved hound issues

--- bin/proposal_administrator.rb
+++ bin/proposal_administrator.rb
@@ -1,21 +1,21 @@
 class ProposalAdministrator
+  attr_reader :proposals
 
-	attr_reader :proposals
+  def initialize
+    @proposals = []
+  end
 
-	def initialize()
-		@proposals = []
-	end
+  def add_proposal(a_proposal)
+    @proposals << verify_title(a_proposal)
+  end
 
-	def add_proposal(a_proposal)
-		@proposals << verify_title(a_proposal)
-	end
-
-	def verify_title(a_proposal)
-		@proposals.each do |proposal|
-			if proposal.same_title? a_proposal
-				a_proposal.append_author_to_title				
-			end
-		end
-		a_proposal
-	end
+  def verify_title(a_proposal)
+    @proposals.each do |proposal|
+      if proposal.same_title? a_proposal
+        a_proposal.append_author_to_title
+      end
+    end
+    a_proposal
+  end
 end
+
--- spec/proposal_spec.rb
+++ spec/proposal_spec.rb
@@ -2,50 +2,48 @@ require 'rspec'
 require_relative '../bin/proposal'
 
 describe 'proposal' do
-	describe 'initialize' do
-		it 'should initialize with a title, a description and an author' do
-			proposal = Proposal.new 'A title', 'A description', 'An author'
-		end
+  describe 'initialize' do
+    it 'should initialize with a title, a description and an author' do
+      proposal = Proposal.new 'A title', 'A description', 'An author'
+      proposal.title.should eq 'A title'
+      proposal.description.should eq 'A description'
+      proposal.author.should eq 'An author'
+    end
 
-		it 'should be created correctly' do
-			proposal = Proposal.new 'A title', 'A description', 'An author'
-			proposal.title.should eq 'A title'
-			proposal.description.should eq 'A description'
-			proposal.author.should eq 'An author'
-		end
+    it 'should raise an error when title is "as"' do
+      expect { Proposal.new('as', 'A description', 'An author') }.to raise_error
+    end
 
-		it 'should raise an error when title is "as"' do
-			expect {Proposal.new('as', 'A description', 'An author')}.to raise_error()
-		end
+    it 'should raise an error when description is empty' do
+      expect { Proposal.new('A title', '', 'An author') }.to raise_error
+    end
 
-		it 'should raise an error when description is empty' do
-			expect {Proposal.new('A title', '', 'An author')}.to raise_error()
-		end
+    it 'should raise an error when author is "h"' do
+      expect { Proposal.new('A title', 'A description', 'h') }.to raise_error
+    end
+  end
 
-		it 'should raise an error when author is "h"' do
-			expect {Proposal.new('A title', 'A description', 'h')}.to raise_error()
-		end
-	end
+  describe 'same_title' do
+    it 'should be true' do
+      proposal1 = Proposal.new 'A title', 'A description1', 'An author1'
+      proposal2 = Proposal.new 'A title', 'A description2', 'An author2'
+      proposal1.same_title?(proposal2).should eq true
+    end
 
-	describe 'same_title' do
-		it 'should be true' do
-			proposal1 = Proposal.new 'A title', 'A description1', 'An author1'
-			proposal2 = Proposal.new 'A title', 'A description2', 'An author2'
-			proposal1.same_title?(proposal2).should eq true
-		end
+    it 'should be false' do
+      proposal1 = Proposal.new 'A title1', 'A description1', 'An author1'
+      proposal2 = Proposal.new 'A title2', 'A description2', 'An author2'
+      proposal1.same_title?(proposal2).should eq false
+    end
+  end
 
-		it 'should be false' do
-			proposal1 = Proposal.new 'A title1', 'A description1', 'An author1'
-			proposal2 = Proposal.new 'A title2', 'A description2', 'An author2'
-			proposal1.same_title?(proposal2).should eq false
-		end
-	end
-
-	describe 'append_author_to_title' do
-		it 'should have the title "A title - An author" when the title is "A title" and author is "An author"' do
-			proposal = Proposal.new 'A title', 'A description', 'An author'
-			proposal.append_author_to_title
-			proposal.title.should eq 'A title - An author'
-		end
-	end
+  describe 'append_author_to_title' do
+    it 'should have the title "A title - An author" when the title is "A title"
+        and author is "An author"' do
+      proposal = Proposal.new 'A title', 'A description', 'An author'
+      proposal.append_author_to_title
+      proposal.title.should eq 'A title - An author'
+    end
+  end
 end
+
