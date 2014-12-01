b1d6266d0eadbed5d29682db02040a9be4361d30
hachedeeme <hdmaidana@gmail.com>
2014-11-30 23:20:54 -0300

    US-006 - Reconocimiento automatico del Usuario con las propuestas y los comentarios

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -11,9 +11,8 @@ class Proposal
   property :description, Text,   required: true, :length => 1..500
   property :author,      String, required: true, :length => 3..50
   property :date,        DateTime
+  has n,   :comments
   has_tags_on :tags
-  # has n, :tags, :through => Resource
-  # has n,   :comments
 
   def same_title?(a_proposal)
     self.title.eql? a_proposal.title
@@ -22,11 +21,5 @@ class Proposal
   def append_author_to_title
     self.title += (" - " + @author)
   end
-
-  # def tag!(tags)
-  #   tags = tags.split(" ").map do |tag|
-  #   Tag.find_or_create_by_label(tag)
-  #   end
-  #   self.tags << tags
-  # end
+  
 end
