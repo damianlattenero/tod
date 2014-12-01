a78fb2710ce58bd01fed2b8a0f25693427f5a35e
Cravacuore <cravacuore@hotmail.com>
2014-12-01 09:19:31 -0300

    US-006: Tab indentation correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,18 +12,19 @@ class User
   property :provider, String
 
   def self.find_uid(uid)
-  	User.first(:uid => uid)
+    User.first(:uid => uid)
   end
 
   def self.new_from_omniauth(omniauth)
-  	user = User.new
-  	user.uid  = omniauth["uid"]
-  	user.name = omniauth["info"]["nickname"]
-  	user.role = 'user'
-  	user.email = omniauth["info"]["email"]
-  	user.provider = 'github'
-	user.save!
-  	user
+    user = User.new
+    user.uid  = omniauth["uid"]
+    user.name = omniauth["info"]["nickname"]
+    user.role = 'user'
+    user.email = omniauth["info"]["email"]
+    user.provider = 'github'
+  user.save!
+    user
   end
 
 end
+
