ad1fd5c5542d9a5d1045edecf848d37abce2d8bc
javierperini <javierperini90@gmail.com>
2015-06-05 00:38:11 -0300

    #Add: new branch

---
+++ features/step_definitions/proposals_quantity_reports.rb
@@ -0,0 +1,27 @@
+Given(/^there are (\d+) proposals$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^a revisor user visits reports url$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^reports page is displayed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+And(/^selects quantity report$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^quantity report is displayed with quantity (\d+)$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^a non\-revisor user visits reports page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^a not\-found page is displayed$/) do
+  pending # express the regexp above with the code you wish you had
+end
