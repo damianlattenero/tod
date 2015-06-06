311c3133079db2063d6b5098185765a73a400216
Marcelo Rubini <rubini.marcelo@gmail.com>
2015-06-06 18:42:15 -0300

    'then he should not be logged as admin' passed

--- features/step_definitions/admin_login_steps.rb
+++ features/step_definitions/admin_login_steps.rb
@@ -1,7 +1,8 @@
 # encoding: utf-8
 Given(/^a user with mail "(.*?)"$/) do |mail|
-  User.new_user mail
-  expect(User.get(1).email).to eq mail
+  @mail = mail
+  User.new_user @mail
+  expect(User.first(:email => @mail)).not_to eq nil
 end
 
 When(/^logging in$/) do
@@ -9,10 +10,10 @@ When(/^logging in$/) do
   click_link('Iniciar sesión con GitHub')
 end
 
-Then(/^he should be logged as admin$/) do
-  pending # express the regexp above with the code you wish you had
+Then(/^he should not be logged as admin$/) do
+  expect(User.first(:email => @mail)).not_to eq (Role.new :admin)
 end
 
-Then(/^he should not be logged as admin$/) do
-  pending # express the regexp above with the code you wish you had
+Then(/^he should be logged as admin$/) do
+  pending
 end
