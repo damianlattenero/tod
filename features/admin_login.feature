81090879330390d2cd4a3fdc7300b4fd4c7aa5b7
Marcelo Rubini <rubini.marcelo@gmail.com>
2015-06-06 14:35:55 -0300

    feature scenarios + steps added

---
+++ features/admin_login.feature
@@ -0,0 +1,12 @@
+Feature: Admin login
+
+Scenario: user with admin email logs in as admin
+  Given a user with mail "admin@mail.com"
+  When  logging in
+  Then  he should be logged as admin
+
+@wip
+Scenario: user without admin email is not logged in as admin
+  Given a user with mail "not-admin@mail.com"
+  When  logging in
+  Then  he should not be logged as admin
---
+++ features/step_definitions/admin_login_steps.rb
@@ -0,0 +1,15 @@
+Given(/^a user with mail "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^logging in$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^he should be logged as admin$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^he should not be logged as admin$/) do
+  pending # express the regexp above with the code you wish you had
+end
