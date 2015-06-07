1dec03208ab5a5159bcd7e9bd07db4d63b6d23a9
Marcelo Rubini <rubini.marcelo@gmail.com>
2015-06-07 18:56:46 -0300

    admin user validation over email attr

--- lib/admin_manager.rb
+++ lib/admin_manager.rb
@@ -1,6 +1,6 @@
 class AdminManager
   include Singleton
-  attr_accessor :admin_name
+  attr_accessor :admin_name, :admin_email
 
   def self.set_as_admin_if_eligible(some_user)
     logger.debug "LOGGED #{some_user} with name #{some_user.name} where is_admin= #{is_admin(some_user)}"
@@ -11,11 +11,11 @@ class AdminManager
   end
 
   def self.is_admin(some_user)
-    some_user && AdminManager.instance.admin_name == some_user.name
+    some_user && AdminManager.instance.admin_email == some_user.name
   end
 
   def self.set_admin_name(name)
-    AdminManager.instance.admin_name = name
+    AdminManager.instance.admin_email = name
   end
 
 end
