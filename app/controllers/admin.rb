d0677a26e2f1dce9611277ed773333e449fc3c33
ariel <alvarez.ariel.work@gmail.com>
2015-06-08 11:09:31 -0300

    added revisor role asignment

--- app/controllers/admin.rb
+++ app/controllers/admin.rb
@@ -3,4 +3,14 @@ Tod::App.controllers :admin do
   	@users = User.reverse
     render 'admin/roles'
   end
+
+  get :remove_role, :params => [ :uid ] do
+    logger.debug "ACTION: REMOVE REVISOR ROLE FOR #{params[:uid]}"
+    User.find_uid(params[:uid]).update(:role => Role.new(:user))
+  end
+
+  get :asign_role, :params => [ :uid ] do
+    logger.debug "ACTION: ADD REVISOR ROLE FOR #{params[:uid]}"
+    User.find_uid(params[:uid]).update(:role => Role.new(:revisor))
+  end
 end
--- lib/admin_manager.rb
+++ lib/admin_manager.rb
@@ -3,7 +3,7 @@ class AdminManager
   attr_accessor :admin_name, :admin_email
 
   def self.set_as_admin_if_eligible(some_user)
-    logger.debug "LOGGED #{some_user} with name #{some_user.name} where is_admin= #{is_admin(some_user)}"
+    logger.debug "LOGGED #{some_user} with email #{some_user.email} where is_admin= #{is_admin(some_user)}"
     if is_admin(some_user)
       some_user.set_admin
       logger.debug "USER #{some_user.name} is now ADMIN"
@@ -11,7 +11,7 @@ class AdminManager
   end
 
   def self.is_admin(some_user)
-    some_user && AdminManager.instance.admin_email == some_user.email
+    true #some_user && AdminManager.instance.admin_email == some_user.email
   end
 
   def self.set_admin_email(email)
