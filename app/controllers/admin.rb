eaff996e05d238dafd3600c8dfda85038e873493
ariel <alvarez.ariel.work@gmail.com>
2015-06-14 05:37:00 -0300

    US19 Added admin Conference controller

--- app/controllers/admin.rb
+++ app/controllers/admin.rb
@@ -5,7 +5,19 @@ Tod::App.controllers :admin do
     }
   end
 
-  get :roles, :map => '/conference' do with_admin_role{
+  get :conference, :map => '/conference' do with_admin_role{
+    @conference = Conference.first_or_create
+    render 'admin/conference'
+  }
+  end
+
+  post :conference, :map => '/conference/update' do with_admin_role {
+    reviews_per_proposal = params[:conference][:reviews_per_proposal]
+    @conference = Conference.first_or_create
+    @conference.reviews_per_proposal = reviews_per_proposal
+    @conference.save
+
+    @success_message = true
     render 'admin/conference'
   }
   end
