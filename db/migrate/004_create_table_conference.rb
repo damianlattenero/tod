c624818818242ced3d204893fa16405d805421b1
ariel <alvarez.ariel.work@gmail.com>
2015-06-14 05:36:06 -0300

    US19 Added Conference migration

--- db/migrate/004_create_table_conference.rb
+++ db/migrate/004_create_table_conference.rb
@@ -1,12 +1,12 @@
-migration 4, :create_conference do
+migration 4, :create_conferences do
   up do
-    create_table :conference do
+    create_table :conferences do
       column :id, Integer, :serial => true
-      column :reviews_per_proposal, Numeric, required: true, :default => 3
+      column :reviews_per_proposal, DataMapper::Property::Numeric, :default => 3
     end
   end
 
   down do
-    drop_table :conference
+    drop_table :conferences
   end
 end
