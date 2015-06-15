9b001c4ac5415eaf3f8a901a3cf77b56ade8b88b
Marcelo Rubini <rubini.marcelo@gmail.com>
2015-06-14 21:44:52 -0300

    US17 - evaluation opinion added to model

---
+++ app/models/evaluation_opinion.rb
@@ -0,0 +1,27 @@
+class EvaluationOpinion
+
+  def initialize(opinion=:strongly_agreed)
+    @opinion = opinion
+  end
+
+  def is_strongly_agreed?
+    @opinion.equal? :strongly_agreed
+  end
+
+  def is_weakly_agreed?
+    @opinion.equal? :weakly_agreed
+  end
+
+  def is_strongly_rejected?
+    @opinion.equal? :strongly_rejected
+  end
+
+  def is_weakly_rejected?
+    @opinion.equal? :weakly_rejected
+  end
+
+  def to_s
+    @opinion.to_s
+  end
+
+end
