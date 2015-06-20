2ae6f4538bd7468f1fddee33571a799380eb4779
javierperini <javierperini90@gmail.com>
2015-06-20 12:35:21 -0300

    US30 ADD: define_audience_proposal_steps.rb, define_audience_proposal.feature

---
+++ features/step_definitions/define_audience_proposal_steps.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+When(/^an user creates a proposal$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+And(/^selects "(.*?)" as audience’s type$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^audience’s type for proposal should be "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
