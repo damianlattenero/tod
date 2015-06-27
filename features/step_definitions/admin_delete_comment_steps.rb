4a0e98d9df5dc87f02932278c1fb729ad671630c
javierperini <javierperini90@gmail.com>
2015-06-26 23:04:08 -0300

    US43  add: admin_delete_comment_steps.rb, admin_delete_comment.feature

---
+++ features/step_definitions/admin_delete_comment_steps.rb
@@ -0,0 +1,29 @@
+And(/^a proposal with "(.*?)" comment$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^admin user visits the proposals list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+And(/^enters the details of the proposal$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+And(/^click on the delete button of comment "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^it should display "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+And(/^comment "(.*?)" should not appear$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^regular user visits the proposals list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+
