5571b8862cd0a3a9e4950d568e5cef276953857e
Marcelo José <rubini.marcelo@gmail.com>
2015-06-28 18:58:28 -0300

    US44 - feature + undefined steps added

---
+++ features/step_definitions/show_proposal_visits_steps.rb
@@ -0,0 +1,16 @@
+
+Given(/^a proposal with “(\d+)” visits$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^user visits proposal list$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^should see proposal with “(\d+)” visits$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^user enters proposal details$/) do
+  pending # express the regexp above with the code you wish you had
+end
