dc9c60fb0977ea62c3fc243be836149005857001
ariel <alvarez.ariel.work@gmail.com>
2015-06-29 01:44:09 -0300

    Added missing migrations from the beginning of times

---
+++ db/migrate/009_create_user.rb
@@ -0,0 +1,16 @@
+migration 9, :create_user do
+  up do
+    create_table :users do
+      column :id, Integer, :serial => true
+      column :name, DataMapper::Property::String,      :length => 255
+      column :email, DataMapper::Property::String,     :length => 255
+      column :role, DataMapper::Property::RoleMapper
+      column :uid, DataMapper::Property::String,       :length => 255
+      column :provider, DataMapper::Property::String,  :length => 255
+    end
+  end
+
+  down do
+    drop_table :users
+  end
+end
